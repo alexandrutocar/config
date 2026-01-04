2cad7c25772ad10508c5587ed9036ed7c1f13189
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-01-04 01:19:36 +0100

    fix: use shared garbage collection policy (there is no practical benefit to it)

--- etc/aether/settings/nix.nix
+++ etc/aether/settings/nix.nix
@@ -14,13 +14,4 @@
   inherit (lib.lists) singleton;
 in {
   imports = singleton (self + /etc/shared/settings/nix.nix);
-
-  nix = {
-    # nix-collect-garbage --delete-older-than 365d
-    gc = {
-      automatic = true;
-      dates = "06:15";
-      options = "--delete-older-than 365d";
-    };
-  };
 }
