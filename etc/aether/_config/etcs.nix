40e8aa5d4cb90bf9edd2dcf3de0302ca50578335
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-08-27 16:02:36 +0200

    chore(aether): temporarily disable GPU features
    GPU workloads are not currently needed. Configuration can be restored when LLM applications are required.

--- etc/aether/_config/etcs.nix
+++ etc/aether/_config/etcs.nix
@@ -10,8 +10,17 @@
   self,
   lib,
   ...
-}: {
+}: let 
+  inherit (lib.modules) mkForce;
+in  {
   imports = lib.lists.singleton (self + "/etc/shared/01-settings/etcs.nix");
+
+  # ────────────────────────────────────────────────────────────────────────
+  # NOTE: Using integrated graphics until GPU is needed again.
+  #
+  #       Relevant: https://github.com/NixOS/nixpkgs/issues/485579
+  # ────────────────────────────────────────────────────────────────────────
+  hardware.facter.detected.boot.graphics.kernelModules = mkForce ["i915"];
 
   powerManagement.cpuFreqGovernor = "performance";
 
