653a3e619f89e8073ad85f0366dc534d48e93e0b
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-08-27 14:27:34 +0200

    chore(shell): remove Statix patch
    Upstream Statix maintenance has resumed, so the custom patch is no longer needed.

--- nix/dev-shell/default.nix
+++ nix/dev-shell/default.nix
@@ -5,33 +5,17 @@
   ...
 } @ pkgs:
 mkShell {
-  buildInputs = let
-    statix = pkgs.statix.overrideAttrs (_: let
-      src = fetchFromGitHub {
-        owner = "oppiliappan";
-        repo = "statix";
-        rev = "e9df54ce918457f151d2e71993edeca1a7af0132";
-        hash = "sha256-duH6Il124g+CdYX+HCqOGnpJxyxOCgWYcrcK0CBnA2M=";
-      };
-    in {
-      inherit src;
-      cargoDeps = rustPlatform.importCargoLock {
-        lockFile = "${src}/Cargo.lock";
-        allowBuiltinFetchGit = true;
-      };
-    });
-  in
-    with pkgs; [
-      shellcheck
-      alejandra
+  buildInputs = with pkgs; [
+    shellcheck
+    alejandra
 
-      deadnix
-      vulnix
+    deadnix
+    vulnix
 
-      nixd
-      shfmt
-      statix
+    nixd
+    shfmt
+    statix
 
-      opentofu
-    ];
+    opentofu
+  ];
 }
