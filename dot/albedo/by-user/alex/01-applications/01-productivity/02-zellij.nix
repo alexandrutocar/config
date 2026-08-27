874ffc0d5c1f243fc3167778b52f4635b36c96a4
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-08-27 14:22:21 +0200

    feat(alex@albedo): switch to Zellij home-manager module
    Per KP64's recommendation, use Zellij's home-manager module for better integration and configuration management.

--- dot/albedo/by-user/alex/01-applications/01-productivity/02-zellij.nix
+++ dot/albedo/by-user/alex/01-applications/01-productivity/02-zellij.nix
@@ -1,5 +1,23 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    zellij
-  ];
+{pkgs, ...}: let
+  kdl = pkgs.formats.kdl {
+    version = 1;
+  };
+in {
+  programs = {
+    zellij = {
+      enable = true;
+
+      settings = let
+        inherit (kdl.lib) node;
+      in [
+        (node "show_startup_tips" null [false] {} [])
+        (node "default_shell" null ["zsh"] {} [])
+      ];
+
+      plugins = with pkgs.zellijPlugins; [zjstatus];
+
+      exitShellOnExit = true;
+      attachExistingSession = true;
+    };
+  };
 }
