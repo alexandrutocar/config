c5fb004496b51452932a1b27d96b5c44405eae05
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-09-02 00:01:24 +0200

    chore(lumine): refresh facter configuration
    - Regenerate facter report.
    - Remove redundant options that facter already sets.
    - Remove options that do not make sense for a guest host (Hetzner), spec. powerManagement.cpuFreqGovernor, enableRedistributableFirmware.

--- etc/lumine/_config/bootload.nix
+++ etc/lumine/_config/bootload.nix
@@ -3,21 +3,20 @@
 # █▄▄ █▀█ █▀█ ▀█▀ █░░ █▀█ ▄▀█ █▀▄
 # █▄█ █▄█ █▄█ ░█░ █▄▄ █▄█ █▀█ █▄▀
 #
-# bootloader, secure boot...
+# TAGS: Boot, Bootloader, Grub
 #
 # ────────────────────────────────────────────────────────────────────────
-{lib, ...}: let
-  inherit (lib.modules) mkForce;
-in {
-  # BOOTLOADER
-  # ----------
-
+_: {
+  # ────────────────────────────────────────────────────────────────────────
   # NOTE: Hetzner uses BIOS (legacy) boot.
-  boot.loader.systemd-boot.enable = mkForce false;
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_116491725";
-    efiSupport = false;
+  # ────────────────────────────────────────────────────────────────────────
+  boot = {
+    kernelParams = ["boot.shell_on_fail"];
+
+    loader = {
+      grub = {
+        device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_116491725";
+      };
+    };
   };
-  boot.kernelParams = ["boot.shell_on_fail"];
 }
--- etc/lumine/_config/etcs.nix
+++ etc/lumine/_config/etcs.nix
@@ -6,21 +6,6 @@
 # etcs, everything else...
 #
 # ────────────────────────────────────────────────────────────────────────
-{
-  modulesPath,
-  self,
-  ...
-}: {
-  imports = [
-    (self + "/etc/shared/01-settings/etcs.nix")
-    (modulesPath + "/profiles/qemu-guest.nix")
-  ];
-
-  powerManagement.cpuFreqGovernor = "performance";
-
-  hardware = {
-    enableRedistributableFirmware = true;
-    # TODO: Check what it does?
-    # cpu.intel.updateMicrocode = true;
-  };
+{self, ...}: {
+  imports = [(self + "/etc/shared/01-settings/etcs.nix")];
 }
