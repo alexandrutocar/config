274982cf31b98f6be64c711d977ead6429b107ca
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-09-02 17:20:24 +0200

    feat(notes): add security headers

--- etc/lumine/_config/02-containers/01-sigil/02-porti/02-operation/01-proxy/_config/caddy/by-vhost/ueuie.earth/default.nix
+++ etc/lumine/_config/02-containers/01-sigil/02-porti/02-operation/01-proxy/_config/caddy/by-vhost/ueuie.earth/default.nix
@@ -25,6 +25,13 @@ in {
 
         root * ${pkgs.notes}
 
+        # Security Headers
+        header {
+          Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
+          X-Content-Type-Options "nosniff"
+          X-Frame-Options "DENY"
+        }
+
         # Hashed Assets
         header /_astro/* Cache-Control "public, max-age=31536000, immutable"
 
