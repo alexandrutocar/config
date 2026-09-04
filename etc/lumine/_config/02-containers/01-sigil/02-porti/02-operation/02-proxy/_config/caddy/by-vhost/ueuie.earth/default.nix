741cdaf41c865eeba19aa8a90265b34361d2d52b
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-09-04 16:55:28 +0200

    feat: add Onion-Location header
    Enable Onion service discovery.

--- etc/lumine/_config/02-containers/01-sigil/02-porti/02-operation/02-proxy/_config/caddy/by-vhost/ueuie.earth/default.nix
+++ etc/lumine/_config/02-containers/01-sigil/02-porti/02-operation/02-proxy/_config/caddy/by-vhost/ueuie.earth/default.nix
@@ -34,6 +34,10 @@ in {
           import ${notesPackage.caddy.importBlock}
 
           header {
+            Onion-Location http://mqlrkzhjt3yk27vtes7r5r3u4tczj6vxhckg3m7rhuvhyxm22hu325id.onion{path}
+          }
+
+          header {
             Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
             X-Content-Type-Options "nosniff"
             X-Frame-Options "DENY"
