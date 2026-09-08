3960e42a405106150f976c81631731278dd3b777
Alexandru Tocar <alexandru.tocar@outlook.com>
2026-09-08 22:04:40 +0200

    chore(davis): 5.4.3 -> 5.4.4

--- nix/packages/by-name/davis/package.nix
+++ nix/packages/by-name/davis/package.nix
@@ -30,19 +30,19 @@
 }:
 php.buildComposerProject (finalAttrs: {
   pname = "davis";
-  version = "5.4.3";
+  version = "5.4.4";
 
   src = fetchFromGitHub {
     owner = "tchapi";
     repo = "davis";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-QCXjw01uJAt22/Vybm/bgE7GeGj4utwdTbXJ2oIVWRo=";
+    hash = "sha256-XGnBXJhAX1hsLOO/dujdf/EUBmaPmW7Kt5e23pabVdU=";
   };
 
   __structuredAttrs = true;
   strictDeps = true;
 
-  vendorHash = "sha256-pjxQDd/xtv5GwUAA7JhANECh74rkPhYSPcr5RKexlZY=";
+  vendorHash = "sha256-9g3v1/NfFkSNhQPV8SeiWnKsdJb2w2pbJtWxji2TNNU=";
 
   composerNoPlugins = false;
 
